21cfda05e78e1f76261f834fbaa12070fa7193fd
Working together <s278018@studenti.polito.it>
2020-10-05 21:17:13 +0200

    Added BoothMul to the project

---
+++ Files/DLX_vhd/DATAPATH/multiplier/constants.vhd
@@ -0,0 +1,14 @@
+package CONSTANTS is
+   constant IVDELAY : time := 0.1 ns;
+   constant NDDELAY : time := 0.2 ns;
+   constant NDDELAYRISE : time := 0.6 ns;
+   constant NDDELAYFALL : time := 0.4 ns;
+   constant NRDELAY : time := 0.2 ns;
+   constant DRCAS : time := 0 ns;
+   constant DRCAC : time := 0 ns;
+   constant numBit : integer := 32;	
+   constant tp_mux : time := 0.5 ns; 
+   constant NBIT_PER_BLOCK : integer := 4;
+   --constant NBLOCKS	: integer := 8;
+   constant NBIT: integer := 32;
+end CONSTANTS;
---
+++ Files/DLX_vhd/DATAPATH/multiplier/sign_ext.vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use WORK.constants.all;
+
+entity sign_ext is 
+	generic ( NBIT: integer := 32);
+	port ( I: in std_logic_vector(NBIT-1 downto 0);
+		   O: out std_logic_vector(2*NBIT-1 downto 0) );
+end sign_ext;
+
+architecture struct of sign_ext is
+
+	component MUX21_GENERIC is
+	generic(NBIT: integer := numBit);
+	Port (	A:	In	std_logic_vector(NBIT-1 downto 0);
+		B:	In	std_logic_vector(NBIT-1 downto 0);
+		SEL:	In	std_logic;
+		Y:	Out	std_logic_vector(NBIT-1 downto 0));
+	end component MUX21_GENERIC;
+
+	signal zeros, ones : std_logic_vector(NBIT-1 downto 0);
+begin
+	zeros <= (others => '0');
+	ones <= (others => '1');
+	mx: MUX21_GENERIC generic map(NBIT) port map ( ones, zeros, I(NBIT-1), O(2*NBIT-1 downto NBIT));
+	O(NBIT-1 downto 0) <= I;
+end struct;
