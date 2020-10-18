7073b761b07b891d3ef10407fc8eb143f5788cf0
Working together <s278018@studenti.polito.it>
2020-10-18 18:43:20 +0200

    XO2 structural

--- Files/DLX_vhd/DLX/DATAPATH/logic_gates/xor2.vhd
+++ Files/DLX_vhd/DLX/DATAPATH/logic_gates/xor2.vhd
@@ -11,9 +11,26 @@ end entity;
 
 architecture df of XOR2 is
 
+	component ND2 is
+		Port (	A:	In	std_logic;
+			B:	In	std_logic;
+			Y:	Out	std_logic);
+	end component ND2;
 
+	component IV is
+		Port (	A:	In	std_logic;
+			Y:	Out	std_logic);
+	end component IV;
 
+	signal B_n, A_n, nd1_out, nd2_out: std_logic;
 
 begin
-	C <= A xor B after XORDELAY;
+
+	A_neg: IV port map(A, A_n);
+	B_neg: IV port map(B, B_n);
+	nd1_logic: ND2 port map(A, B_n, nd1_out);
+	nd2_logic: ND2 port map(A_n, B, nd2_out);
+	nd_result: ND2 port map(nd1_out, nd2_out, C);
+
+	--C <= A xor B after XORDELAY;
 end architecture;
